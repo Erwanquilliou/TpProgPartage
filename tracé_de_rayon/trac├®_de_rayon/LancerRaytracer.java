b9fe996d5c9c50003bfbb0867d42a30a96e78692
Nilecram <marcelin.ray@protonmail.com>
2025-05-22 11:17:44 +0200

    modif apporté

--- tracé_de_rayon/trac├®_de_rayon/LancerRaytracer.java
+++ tracé_de_rayon/trac├®_de_rayon/LancerRaytracer.java
@@ -8,7 +8,7 @@ import raytracer.Image;
 public class LancerRaytracer {
 
     public static String aide = "Raytracer : synthèse d'image par lancé de rayons (https://en.wikipedia.org/wiki/Ray_tracing_(graphics))\n\nUsage : java LancerRaytracer [fichier-scène] [largeur] [hauteur]\n\tfichier-scène : la description de la scène (par défaut simple.txt)\n\tlargeur : largeur de l'image calculée (par défaut 512)\n\thauteur : hauteur de l'image calculée (par défaut 512)\n";
-     
+
     public static void main(String args[]){
 
         // Le fichier de description de la scène si pas fournie
@@ -16,7 +16,7 @@ public class LancerRaytracer {
 
         // largeur et hauteur par défaut de l'image à reconstruire
         int largeur = 512, hauteur = 512;
-        
+
         if(args.length > 0){
             fichier_description = args[0];
             if(args.length > 1){
@@ -27,34 +27,49 @@ public class LancerRaytracer {
         }else{
             System.out.println(aide);
         }
-        
-   
+
+
         // création d'une fenêtre 
         Disp disp = new Disp("Raytracer", largeur, hauteur);
-        
+
         // Initialisation d'une scène depuis le modèle 
         Scene scene = new Scene(fichier_description, largeur, hauteur);
-        
+
         // Calcul de l'image de la scène les paramètres : 
         // - x0 et y0 : correspondant au coin haut à gauche
         // - l et h : hauteur et largeur de l'image calculée
         // Ici on calcule toute l'image (0,0) -> (largeur, hauteur)
-        
+
         int x0 = 0, y0 = 0;
-        int l = largeur, h = hauteur;
-                
+        //on divise l'image en deux
+        int l = largeur/2, h = hauteur/2;
+
         // Chronométrage du temps de calcul
         Instant debut = Instant.now();
         System.out.println("Calcul de l'image :\n - Coordonnées : "+x0+","+y0
-                           +"\n - Taille "+ largeur + "x" + hauteur);
+                +"\n - Taille "+ largeur + "x" + hauteur);
         Image image = scene.compute(x0, y0, l, h);
         Instant fin = Instant.now();
 
         long duree = Duration.between(debut, fin).toMillis();
-        
+
         System.out.println("Image calculée en :"+duree+" ms");
-        
+
+        //compute la deuxieme partie (bas droite)
+        int x02 = largeur/2;
+        int y02 = hauteur/2;
+        l = largeur/2;
+        h = hauteur/2;
+        System.out.println("Calcul de l'image :\n - Coordonnées : "+x0+","+y0
+                +"\n - Taille "+ largeur + "x" + hauteur);
+        debut = Instant.now();
+        Image image2 = scene.compute(x02, y02, l, h);
+        fin = Instant.now();
+        duree = Duration.between(debut, fin).toMillis();
+        System.out.println("Image calculée en :"+duree+" ms");
+
         // Affichage de l'image calculée
         disp.setImage(image, x0, y0);
-    }	
+        disp.setImage(image2, x02, y02);
+    }
 }
